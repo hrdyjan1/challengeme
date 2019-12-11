61c35b81320de7ac12ae345d6f0fffe590bbc7f2
Jan Hrdý <hrdyjan1@gmail.com>
2019-12-11 20:13:01 +0100

    Created Slash screen and logo

---
+++ app/src/Component.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Component = () => <Text>Challenge yourself to make your life fulfilled!</Text>;
+
+export default Component;
--- src/Component.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function Component() {
-  return <Text>Accept the real Challenge.</Text>;
-}

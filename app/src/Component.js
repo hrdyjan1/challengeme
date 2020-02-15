4d8c6f956459aab43e183255704b24e5b359086c
Jan Hrdý <hrdyjan1@gmail.com>
2020-02-15 20:48:49 +0100

    Updated layout

--- app/src/Component.js
+++ app/src/Component.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const Component = () => <Text>Challenge yourself to make your life fulfilled!</Text>;
+const Component = () => {
+  return <Text>Challenge yourself to make your life fulfilled!</Text>;
+};
 
 export default Component;
+
